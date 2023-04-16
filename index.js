a2afd2b944a950a3ac32118aa78401232f1aebb9
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-16 11:23:26 +0300

    added new classes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+class lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.drink = drink;
+        this.soup = soup;
+    }
+}
+class Dinner {
+    #desert;
+    constructor(salad, soup, entree, desert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree=entree;
+        this.#desert= desert;
+    }
+}
